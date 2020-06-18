a70e64ac40188791e5fb33ae1cdac1cca967f650
Eric Wang <johnericcw@gmail.com>
2020-06-18 15:17:27 -0400

    GET and POST accomplishment API working

--- backend_src/model/accomplishment.py
+++ backend_src/model/accomplishment.py
@@ -11,6 +11,34 @@ class Status(IntEnum):
     DROPPED = 3
 
 
+class Accomplishment:
+    def __init__(self, accomplishment_id, date, description, is_planned, status):
+        self.id = accomplishment_id
+        self.date = date
+        self.description = description
+        self.is_planned = is_planned
+        self.status = status
+
+    @classmethod
+    def query_all_accomplishments(cls, cursor):
+        cursor.execute('SELECT rowid, * FROM accomplishments')
+        # result[1] gets returned as text in ISO8601 format
+        return [cls(result[0], result[1][:len('2020-06-18')], result[2], bool(result[3]), Status(result[4]))
+                for result in cursor.fetchall()]
+
+    def to_json_dict(self):
+        return {
+            'id': self.id,
+            'date': self.date,
+            'description': self.description,
+            'is_planned': self.is_planned,
+            'status': self.status
+        }
+
+    def __str__(self):
+        return f'{self.date}, {self.description}, {self.is_planned}, {self.status}'
+
+
 # date: midnight UTC seconds since epoch
 def create_accomplishment_table(cursor):
     query_string = '''
