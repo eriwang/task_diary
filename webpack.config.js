40d4628c14af7c399fde9f8996c6bf721c095fd1
Eric Wang <johnericcw@gmail.com>
2020-06-22 17:07:11 -0400

    Add browser-sync + webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
 module.exports = {
     mode: 'development',
     entry: './frontend_src/index.js',
@@ -11,5 +13,13 @@ module.exports = {
         jquery: 'jQuery'
     },
     devtool: 'inline-source-map',
-    watch: true
+    watch: true,
+    plugins: [
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 5001,
+            proxy: 'http://localhost:5000',  // flask server,
+            files: ['./backend_src/templates/*']
+        })
+    ]
 };
