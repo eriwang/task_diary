a8a32485a7655152e5f17f490c803128d57a1077
Eric Wang <johnericcw@gmail.com>
2020-06-29 17:39:28 -0400

    Bump package for windows, bat deploy script working

--- scripts/build_prod.bat
+++ scripts/build_prod.bat
@@ -1,4 +1,2 @@
-# npx webpack --config webpack.prod.js
-pyinstaller --add-data "backend_src/templates;templates" \
-            --add-data "backend_src/static_gen;static_gen" \
-            backend_src/app.py --noconfirm -F
+call npx webpack --config webpack.prod.js
+call pyinstaller --add-data "backend_src/templates;templates" --add-data "backend_src/static_gen;static_gen" backend_src/app.py --noconfirm -F
